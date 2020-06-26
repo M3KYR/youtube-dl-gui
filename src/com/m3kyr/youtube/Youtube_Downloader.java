081353f2571a5d2d8a4d9767f692390660131f70
M3KYR <pjgomez_1997@tutanota.com>
2020-06-26 15:00:37 +0200

    Code clean up

--- src/com/m3kyr/youtube/Youtube_Downloader.java
+++ src/com/m3kyr/youtube/Youtube_Downloader.java
@@ -14,167 +14,107 @@ public class Youtube_Downloader {
     private JRadioButton audioRadioButton;
     private JRadioButton playlistRadioButton;
     private JRadioButton videoRadioButton;
-    private JTextField pathField;
-    private JTextField urlField;
+    private JTextField pathField = new JTextField("Ruta del video...", 20);
+    private JTextField urlField = new JTextField("URL del video...", 20);
     private JButton downloadButton;
     private JTextArea textArea;
-    private JFileChooser fileSave;
+    private JFileChooser fileSave = new JFileChooser();
 
     public static void main(String[] args) {
-        Youtube_Downloader downloader = new Youtube_Downloader();
-        downloader.go();
+        new Youtube_Downloader().go();
     }
 
     private void go() {
+        setupFrame();
+    }
+
+    private void setupFrame() {
         frame = new JFrame("Youtube Downloader");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JLabel urlLabel = new JLabel("URL");
-
-        urlField = new JTextField("URL del video...",20);
-
-        JLabel pathLabel = new JLabel("Ruta");
-
-        pathField = new JTextField("Ruta del video...",20);
-
-        JButton exploreButton = new JButton("Explorar");
-        exploreButton.addActionListener(new exploreButtonActionListener());
-
-        audioRadioButton = new JRadioButton("Audio");
-        audioRadioButton.addActionListener(new audioRadioButtonActionListener());
-
-        playlistRadioButton = new JRadioButton("Playlist");
-        playlistRadioButton.addActionListener(new playlistRadioButtonActionListener());
-
-        videoRadioButton = new JRadioButton("Video");
-        videoRadioButton.addActionListener(new videoRadioButtonActionListener());
-
-        downloadButton = new JButton("Descargar");
-        downloadButton.setSize(10,10);
-        downloadButton.addActionListener(new downloadButtonActionListener());
-        downloadButton.setEnabled(false);
-
-        textArea = new JTextArea(12,40);
-        textArea.setLineWrap(true);
-        textArea.setEditable(false);
-
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));
-
-        JPanel urlPanel = new JPanel();
-        urlPanel.setLayout(new FlowLayout());
-        urlPanel.setMaximumSize(new Dimension(500,50));
-
-        urlPanel.add(urlLabel);
-        urlPanel.add(urlField);
-
-        JPanel pathPanel = new JPanel();
-        pathPanel.setLayout(new FlowLayout());
-        pathPanel.setMaximumSize(new Dimension(800,50));
-
-        pathPanel.add(pathLabel);
-        pathPanel.add(pathField);
-        pathPanel.add(exploreButton);
-
-        JPanel typePanel = new JPanel();
-        typePanel.setLayout(new FlowLayout());
-        typePanel.setMaximumSize(new Dimension(300,50));
-
-        typePanel.add(audioRadioButton);
-        typePanel.add(videoRadioButton);
-        typePanel.add(playlistRadioButton);
-
-        JPanel downloadPanel = new JPanel();
-        downloadPanel.setLayout(new FlowLayout());
-        downloadPanel.setMaximumSize(new Dimension(100,50));
-
-        downloadPanel.add(downloadButton);
-
-        JPanel outputPanel = new JPanel();
-        outputPanel.setLayout(new FlowLayout());
-        outputPanel.setMaximumSize(new Dimension(600,500));
-
-        outputPanel.add(scrollPane);
-
-        mainPanel.add(urlPanel);
-        mainPanel.add(pathPanel);
-        mainPanel.add(typePanel);
-        mainPanel.add(downloadPanel);
-        mainPanel.add(outputPanel);
-
-        frame.getContentPane().add(BorderLayout.CENTER,mainPanel);
-        frame.setSize(500,400);
+        frame.getContentPane().add(BorderLayout.CENTER, setupMainPanel());
+        frame.setSize(500, 400);
         frame.setVisible(true);
     }
 
-    private class downloadButtonActionListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            if(isValidURL()) {
-                switch (getSelectedOption()) {
-                    case 0:
-                        download(new String[]
-                                {"/bin/zsh",
-                                "-c",
-                                "youtube-dl -x --audio-format 'mp3' --audio-quality 0 -o  '"
-                                        + pathField.getText()
-                                        + "/%(title)s.%(ext)s' '"
-                                        + urlField.getText()
-                                        + "'"});
-                        break;
-                    case 1:
-                        download(new String[]
-                                {"/bin/zsh",
-                                "-c",
-                                "youtube-dl -i -x --audio-format 'mp3' --audio-quality 0 -o  '"
-                                        + pathField.getText()
-                                        + "/%(playlist)s/%(title)s.%(ext)s' '"
-                                        + urlField.getText()
-                                        + "'"});
-                        break;
-                    case 2:
-                        download(new String[]
-                                {"/bin/zsh",
-                                "-c",
-                                "youtube-dl --recode-video mp4 -o '"
-                                        + pathField.getText()
-                                        + "/%(title)s.%(ext)s' '"
-                                        + urlField.getText()
-                                        + "'"});
-                        break;
-                    default:
-                        throw new IllegalStateException("Unexpected value: " + getSelectedOption());
-                }
-            }
-            else {
-                JOptionPane.showMessageDialog(frame,"URL no válida","Invalid URL",JOptionPane.ERROR_MESSAGE);
-            }
-        }
+    private JPanel setupMainPanel() {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.add(setupURLPanel());
+        mainPanel.add(setupPathPanel());
+        mainPanel.add(setupTypePanel());
+        mainPanel.add(setupDownloadPanel());
+        mainPanel.add(setupOutputPanel());
+        return mainPanel;
+    }
+
+    private JPanel setupURLPanel() {
+        JPanel urlPanel = new JPanel();
+        urlPanel.setLayout(new FlowLayout());
+        urlPanel.setMaximumSize(new Dimension(500, 50));
+        urlPanel.add(new JLabel("URL"));
+        urlPanel.add(urlField);
+        return urlPanel;
+    }
+
+    private JPanel setupPathPanel() {
+        JPanel pathPanel = new JPanel();
+        pathPanel.setLayout(new FlowLayout());
+        pathPanel.setMaximumSize(new Dimension(800, 50));
+        pathPanel.add(new JLabel("Ruta"));
+        pathPanel.add(pathField);
+        pathPanel.add(setupExploreButton());
+        return pathPanel;
+    }
+
+    private JButton setupExploreButton() {
+        JButton exploreButton = new JButton("Explorar");
+        exploreButton.addActionListener(new exploreButtonActionListener());
+        return exploreButton;
     }
 
     private class exploreButtonActionListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            fileSave = new JFileChooser();
             fileSave.setDialogTitle("Guardar en");
             fileSave.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
             fileSave.showSaveDialog(frame);
-            if(filePathSelected()) {
+            if (filePathSelected()) {
                 pathField.setText(fileSave.getSelectedFile().toString());
-                if(fileTypeSelected()) {
+                if (fileTypeSelected()) {
                     downloadButton.setEnabled(true);
                 }
             }
         }
     }
 
+    private boolean fileTypeSelected() {
+        return audioRadioButton.isSelected() || videoRadioButton.isSelected() || playlistRadioButton.isSelected();
+    }
+
+    private boolean filePathSelected() {
+        return fileSave.getSelectedFile() != null;
+    }
+
+    private JPanel setupTypePanel() {
+        JPanel typePanel = new JPanel();
+        typePanel.setLayout(new FlowLayout());
+        typePanel.setMaximumSize(new Dimension(300, 50));
+        audioRadioButton = new JRadioButton("Audio");
+        audioRadioButton.addActionListener(new audioRadioButtonActionListener());
+        playlistRadioButton = new JRadioButton("Playlist");
+        playlistRadioButton.addActionListener(new playlistRadioButtonActionListener());
+        videoRadioButton = new JRadioButton("Video");
+        videoRadioButton.addActionListener(new videoRadioButtonActionListener());
+        typePanel.add(audioRadioButton);
+        typePanel.add(videoRadioButton);
+        typePanel.add(playlistRadioButton);
+        return typePanel;
+    }
+
     private class audioRadioButtonActionListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             videoRadioButton.setSelected(false);
             playlistRadioButton.setSelected(false);
-            if(filePathSelected()) {
+            if (filePathSelected()) {
                 downloadButton.setEnabled(true);
             }
         }
@@ -184,7 +124,7 @@ public class Youtube_Downloader {
         public void actionPerformed(ActionEvent e) {
             audioRadioButton.setSelected(false);
             videoRadioButton.setSelected(false);
-            if(filePathSelected()) {
+            if (filePathSelected()) {
                 downloadButton.setEnabled(true);
             }
         }
@@ -194,37 +134,88 @@ public class Youtube_Downloader {
         public void actionPerformed(ActionEvent e) {
             audioRadioButton.setSelected(false);
             playlistRadioButton.setSelected(false);
-            if(filePathSelected()) {
+            if (filePathSelected()) {
                 downloadButton.setEnabled(true);
             }
         }
     }
 
-    private int getSelectedOption() {
-        if(audioRadioButton.isSelected()) {
-            return 0;
-        }
-        else if(playlistRadioButton.isSelected()) {
-            return 1;
-        }
-        else {
-            return 2;
+    private JPanel setupDownloadPanel() {
+        JPanel downloadPanel = new JPanel();
+        downloadPanel.setLayout(new FlowLayout());
+        downloadPanel.setMaximumSize(new Dimension(100, 50));
+        downloadButton = setupDownloadButton();
+        downloadPanel.add(downloadButton);
+        return downloadPanel;
+    }
+
+    private JButton setupDownloadButton() {
+        JButton downloadButton = new JButton("Descargar");
+        downloadButton.setSize(10, 10);
+        downloadButton.addActionListener(new downloadButtonActionListener());
+        downloadButton.setEnabled(false);
+        return downloadButton;
+    }
+
+    private class downloadButtonActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (isValidURL()) {
+                switch (getSelectedOption()) {
+                    case 0:
+                        download(new String[]
+                                {"/bin/zsh",
+                                        "-c",
+                                        "youtube-dl -x --audio-format 'mp3' --audio-quality 0 -o  '"
+                                                + pathField.getText()
+                                                + "/%(title)s.%(ext)s' '"
+                                                + urlField.getText()
+                                                + "'"});
+                        break;
+                    case 1:
+                        download(new String[]
+                                {"/bin/zsh",
+                                        "-c",
+                                        "youtube-dl -i -x --audio-format 'mp3' --audio-quality 0 -o  '"
+                                                + pathField.getText()
+                                                + "/%(playlist)s/%(title)s.%(ext)s' '"
+                                                + urlField.getText()
+                                                + "'"});
+                        break;
+                    case 2:
+                        download(new String[]
+                                {"/bin/zsh",
+                                        "-c",
+                                        "youtube-dl --recode-video mp4 -o '"
+                                                + pathField.getText()
+                                                + "/%(title)s.%(ext)s' '"
+                                                + urlField.getText()
+                                                + "'"});
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + getSelectedOption());
+                }
+            } else {
+                JOptionPane.showMessageDialog(frame, "URL no válida", "Invalid URL", JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 
-    private boolean fileTypeSelected() {
-        return audioRadioButton.isSelected() || videoRadioButton.isSelected() || playlistRadioButton.isSelected();
-    }
-    private boolean filePathSelected() {
-        return fileSave.getSelectedFile() != null;
-    }
     private boolean isValidURL() {
         try {
             URL url = new URL(urlField.getText());
             return urlField.getText().contains("www.youtube");
-        }
-        catch (MalformedURLException e) {
+        } catch (MalformedURLException e) {
             return false;
+        }
+    }
+
+    private int getSelectedOption() {
+        if (audioRadioButton.isSelected()) {
+            return 0;
+        } else if (playlistRadioButton.isSelected()) {
+            return 1;
+        } else {
+            return 2;
         }
     }
 
@@ -247,9 +238,31 @@ public class Youtube_Downloader {
                 }
             });
             ioThread.start();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    private JPanel setupOutputPanel() {
+        JPanel outputPanel = new JPanel();
+        outputPanel.setLayout(new FlowLayout());
+        outputPanel.setMaximumSize(new Dimension(600, 500));
+        outputPanel.add(setupScrollPane());
+        return outputPanel;
+    }
+
+    private JScrollPane setupScrollPane() {
+        setupTextArea();
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        return scrollPane;
+    }
+
+    private void setupTextArea() {
+        textArea = new JTextArea(12, 40);
+        textArea.setLineWrap(true);
+        textArea.setEditable(false);
+    }
+
 }
